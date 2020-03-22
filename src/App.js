6be4e288881368c3bbcc7323fed703b4f034427a
Golden Eagle <joowoon.kang@biola.edu>
2020-03-21 20:02:28 -0700

    implement a search bar that filters by names

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField:""
     };
   }
 
@@ -18,9 +19,25 @@ class App extends Component {
  
 
   render(){
+    const { monsters, searchField} = this.state; 
+    //line 24,25 are equlvalent to 22. 
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField; 
+    // console.log(monsters)
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
      return (
     <div className="App">
-      <CardList monsters={this.state.monsters} /> 
+      <input 
+        type='serach' 
+        placeholder="serch monsters" 
+        onChange={e => {
+          this.setState({ searchField: e.target.value});
+
+        }} />
+
+      <CardList monsters = {filteredMonsters} /> 
      
     </div>
   );
